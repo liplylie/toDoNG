c4a6ecb39829f0fe94dbc80c1e98bcc70369f266
Jordan Daniels <decentpianist@gmail.com>
2017-11-16 17:58:43 -0800

    initialized ng app todo

---
+++ client/static/components/navTab.js
@@ -0,0 +1,5 @@
+angular.module('todo')
+.component('navTab', {
+  // TODO
+   templateUrl: './templates/navTab.html',
+});
--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 
-
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname,'../client/static')))
 
 
