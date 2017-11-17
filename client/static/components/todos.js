c21a8a8c1aefb776c48a2c61284f899b72e59f00
Jordan Daniels <decentpianist@gmail.com>
2017-11-16 23:52:35 -0800

    delete functionality done

--- client/static/components/todos.js
+++ client/static/components/todos.js
@@ -4,6 +4,9 @@ angular.module('todo')
 		this.todos = [];
 		this.addTodo = function(todo){
 			this.todos.push(todo)
+		},
+		this.deleteTodo = function(todo){
+			this.todos.splice(this.todos.indexOf(todo), 1)
 		}
 	},
 	templateUrl: './templates/todos.html'
